0538128a2fa4477e50b9f27705c9a3775d7e6b95
Roman Larionov <pcsaveddotcom@gmail.com>
2024-03-23 16:27:48 -0400

    lab after break - 2 / associates

---
+++ associates/views.py
@@ -0,0 +1,8 @@
+from .models import Person
+from rest_framework import viewsets, permissions
+from .serializers import PersonSerializer
+
+class PersonViewSet(viewsets.ModelViewSet):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+    permission_classes = [permissions.AllowAny]
